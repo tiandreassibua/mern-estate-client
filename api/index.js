ba62f85afb6973446e03ef9a5fc3ccae4377dae3
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-11-19 22:00:59 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`MongoDB connected`);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 app.listen(5000, () => {
