d55b323718c440b2e2d7c5d23a6d4f568bcae762
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-11-20 07:49:10 +0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-import { hashPassword } from "../utils/helper.js";
+import {
+    comparePassword,
+    generateToken,
+    hashPassword,
+} from "../utils/helper.js";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -9,6 +13,27 @@ export const signup = async (req, res, next) => {
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "Invalid credentials"));
+
+        const validPassword = comparePassword(password, validUser.password);
+        if (!validPassword)
+            return next(errorHandler(404, "Invalid credentials"));
+
+        const token = generateToken({ id: validUser._id });
+        const { password: pass, ...user } = validUser._doc;
+
+        res.cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(user);
     } catch (error) {
         next(error);
     }
--- api/utils/helper.js
+++ api/utils/helper.js
@@ -1,5 +1,14 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const hashPassword = (password) => {
     return bcryptjs.hashSync(password, 10);
+};
+
+export const comparePassword = (password, hashedPassword) => {
+    return bcryptjs.compareSync(password, hashedPassword);
+};
+
+export const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET);
 };
